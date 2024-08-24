4ff605a27112b48bd19ad57d70de83ea23e3dd34
NEELA SAIKIRAN <neelasaikiran123@gmail.com>
2024-08-24 21:05:43 +0530

    adding files with API

---
+++ key.js
@@ -0,0 +1,3 @@
+// Adding the key which is received to the mail from open movie database API
+
+key = "https://www.omdbapi.com/?i=tt3896198&apikey=ef4d6a58";
---
+++ script.js
@@ -0,0 +1,8 @@
+// Initial references
+
+let movieNameRef = document.getElementById('movie-name');
+let searchBtn = document.getElementById('searchBtn')  ;
+let result = document.getElementById('result');
+
+
+// Functions to fetch data from API
